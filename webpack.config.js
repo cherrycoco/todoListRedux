eb1ae2df93f2e851448f3d9d9c9ba5faea99e86e
Cherry Xu <cherryxuqianyun@gmail.com>
2018-03-04 17:50:18 -0800

    first commit - webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, './client/src');
+const DIST_DIR = path.join(__dirname, './client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['react', 'es2015', 'state-2']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'stage-2']
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
