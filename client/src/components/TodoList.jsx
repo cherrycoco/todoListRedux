da8e7ac96708631d019a682e8ce4b7f65dd4ceeb
Cherry Xu <cherryxuqianyun@gmail.com>
2018-03-24 22:23:50 -0700

    TodoList Complete

--- client/src/components/TodoList.jsx
+++ client/src/components/TodoList.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import Todo from './Todo.jsx';
 
-const Todos = (props) => {
-  
-}
+const TodoList = ({ todos, onTodoClick }) => (
+  <ul>
+    {todos.map(todo => 
+      <Todo 
+        key={todo.id} 
+        {...todo} 
+        onClick={() => onTodoClick(todo.id)} 
+      />
+    )}
+  </ul>
+)
+
+export default TodoList;
